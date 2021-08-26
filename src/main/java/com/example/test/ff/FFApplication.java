4e28b364f0610849e72da8d80f73e8e155971058
wpfok <wpfok1314@163.com>
2021-08-26 09:56:24 +0800

    testDemo

---
+++ src/main/java/com/example/test/ff/FFApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.ff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FFApplication.class, args);
+    }
+
+}
